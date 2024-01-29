b101f23b328315401b98b378a035e1aaf5b1b4d9
장현준 <50596733+gapple95@users.noreply.github.com>
2024-01-30 01:41:00 +0900

    20140130 스터디

---
+++ 개인/Java Projects/JAVA_1/src/baekjoon/P1914.java
@@ -0,0 +1,15 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class P1914 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner s = new Scanner(System.in);
+		
+		int n = s.nextInt();
+	}
+
+}
