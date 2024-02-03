eda1bbccf0f08ed4c519fb974c16c985532963de
장현준 <50596733+gapple95@users.noreply.github.com>
2024-02-04 03:32:08 +0900

    도영이카드게임 완

---
+++ 개인/Java Projects/JAVA_1/src/algo0201/Solution_2001_파리퇴치_장현준.java
@@ -0,0 +1,85 @@
+package algo0201;
+
+import java.util.Scanner;
+
+/*
+ * M*M크기의 파리채를 한 번 내리쳐 최대한 많은 파리를 죽이고자 한다.
+ * 
+ * @제약 사항
+ * N은 5이상 15이하
+ * M은 2이상 N이하
+ * 각 영역의 파리 갯수는 30이하 이다.
+ * 
+ * @입력
+ * 가장 첫 줄에는 테스트 케이스 T
+ * 각 테스트 케이스는 첫 번째 줄에 N과 M이 주어지고,
+ * 다음 N 줄에 걸쳐 N * N 배열이 주어진다.
+ * 
+ * @출력
+ * 출력의 각 줄은 "#t"로 시작하고, 공백을 한 칸 둔 다음
+ * 정답을 출력
+ * 정답은 최대한 M*M으로 잡을 수 있는 최대파리
+ * 
+ * @해결방안
+ * 한줄씩 읽는 배열(구간합5 방식)을 구성하여 필요한 지점만 더한다.
+ * 
+ */
+
+public class Solution_2001_파리퇴치_장현준 {
+
+	static int M,N;
+	static int[][] map;
+	static int[][] atk;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner s = new Scanner(System.in);
+		
+		int T = s.nextInt();
+		for(int t=1; t<=T; t++) {
+			N = s.nextInt();
+			M = s.nextInt();
+			
+			map = new int[N+1][N+1];
+			atk = new int[M][M];
+			
+			int[][] map2 = new int[N+1][N+1];
+			int sum;
+			for (int i = 1; i <= N; i++) {
+				sum = 0;
+				for (int j = 1; j <= N; j++) {
+					map[i][j] = s.nextInt();
+					sum += map[i][j];
+					map2[i][j] = sum;
+				}
+			}
+			
+//			// 한줄 합계 테이블
+//			for (int i = 0; i <= N; i++) {
+//				for (int j = 0; j <= N; j++) {
+//					System.out.print(map2[i][j] + "\t");
+//				}
+//				System.out.println();
+//			}
+			
+			int max = Integer.MIN_VALUE;
+			for(int i=M; i<=N; i++) {
+				for(int j=M; j<=N; j++) {
+					sum = 0;
+					for(int k=i-M+1; k<=i; k++) {
+//						System.out.printf("i : %d, j : %d , k : %d\n",i,j,k);
+						sum+=map2[k][j];
+						sum-=map2[k][j-M];
+					}
+					if(max < sum) {
+						max = sum;
+					}
+				}
+			}
+			System.out.printf("#%d %d\n",t, max);
+		}
+	}
+
+	
+}
--- 개인/Java Projects/JAVA_1/src/programmers/Joystick.java
+++ 개인/Java Projects/JAVA_1/src/programmers/Joystick.java
@@ -1,5 +1,6 @@
 package programmers;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 /*
@@ -35,38 +36,163 @@ import java.util.Scanner;
  * 만들고자 하는 name을 만들때 조이스틱 횟수
  * 
  * @해결방안
- * 
+ * 먼저 첫번째 0번 인덱스를 확인 후 맞는 name 만들기
+ * 가장 가까운 위치의 name을 찾아가기 => 범위 확인 함수
+ * 이동
  * 
  */
 
 public class Joystick {
-	
+
 	static int middleAlpa = "Z".charAt(0) - "A".charAt(0);
 
+	static char[] names;
+	static String name;
+	static int cur;
+	static int count;
+	static int S; // 단어 길이
+	static boolean LR = true; // true면 left, false면 right
+
+	// A : 65, Z : 90
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		 Scanner s = new Scanner(System.in);
-		 
-		 String name = s.nextLine();
-		 System.out.println(name);
-		 
-		 System.out.println(middleAlpa);
+		Scanner s = new Scanner(System.in);
+
+		name = s.nextLine();
+		names = new char[name.length()];
+
+		System.out.println(name);
+
+		for (int i = 0; i < name.length(); i++) {
+			names[i] = 'A';
+		}
+
+		S = name.length() - 1;
+
+		while (!name.equals(String.valueOf(names))) {
+			if (names[cur] != name.charAt(cur)) { // 현재 있는 곳이 완성되었다면 다른곳으로 이동
+				// 어디가 더 가까이 빨리 A~Z으로 변환하는지
+//				if (check(name.charAt(cur))) {
+//					names[cur] = Up(names[cur]);
+//				} else {
+//					names[cur] = Down(names[cur]);
+//				}
+				names[cur] = Down(names[cur]);
+			} else {
+				int i = checkLR(name.charAt(cur)); // 몇번을 옆으로 갈지 알려준다.
+				while (i > 0) {
+					if (LR) {
+						Left();
+					} else {
+						Right();
+					}
+					i--;
+				}
+			}
+			System.out.println(Arrays.toString(names));
+		}
+		System.out.println(count);
 	}
 
-	public static void Up() {
-		
+	/*
+	 * 위키) 다음 알파벳(Z 다음 A) 아래키) 이전 알파벳(A에서 아래쪽이면 Z) 왼쪽키) 커서를 왼쪽으로 이동(첫 번째에서 왼쪽은 마지막)
+	 * 오른쪽키) 커서를 오른쪽으로 이동(마지막에서 오른쪽은 처음)
+	 */
+	public static char Up(char c) {
+		count++;
+		if (c == 'Z')
+			return 'A';
+		else {
+			return (char) (c + 1);
+		}
 	}
-	
-	public static void Down() {
-		
+
+	public static char Down(char c) {
+		count++;
+		if (c == 'A')
+			return 'Z';
+		else {
+			return (char) (c - 1);
+		}
 	}
-	
+
 	public static void Left() {
-		
+		count++;
+		if (cur - 1 < 0)
+			cur = S;
+		else
+			cur--;
 	}
-	
+
+	public static boolean LeftCheck(int n) { // n>=1
+		int tmp_cur = n % (S + 1); // S개 한바퀴를 돌면 제자리로 온다.
+		if (cur - tmp_cur < 0) { // 만약 0번에서 2(n)칸 뒤로가면 24(26-n)가 되어야한다.
+			return names[S - tmp_cur] == name.charAt(S - tmp_cur);
+		} else {
+			// 범위를 벗어나지 않는 선에서 계산된 tmp_cur 만큼 왼쪽 이동
+			return names[cur - tmp_cur] == name.charAt(cur - tmp_cur);
+		}
+	}
+
 	public static void Right() {
+		count++;
+		if (cur + 1 > S)
+			cur = 0;
+		else
+			cur++;
+	}
+
+	public static boolean RightCheck(int n) { // n>=1
+		int tmp_cur = n % (S + 1); // S개 한바퀴를 돌면 제자리로 온다.
+		if (cur + tmp_cur > S) // 만약에 25번에서 2(n)칸 앞으로 가면 1(0+n-1)번으로 가야한다.
+			return names[0 + tmp_cur - 1] == name.charAt(0 + tmp_cur - 1);
+		else {
+			// 범위를 벗어나지 않는 선에서 계산된 tmp_cur 만큼 오른쪽 이동
+			return names[cur + tmp_cur] == name.charAt(cur + tmp_cur);
+		}
+	}
+
+	//
+	public static boolean UpCheck(int n) {
+		return true;
+	}
+
+	// true면 Up, false Down
+	public static boolean checkUD(char c) {
+		boolean result = true;
 		
+//		int i=0;
+//		while(true) {
+//			if()
+//		}
+		
+		return result;
+	}
+
+	// true면 Left, false Right
+	public static int checkLR(char c) {
+		int i = 1;
+		int tmp_left = cur;
+		int tmp_right = cur;
+		while (true) {
+			tmp_left += -1;
+			tmp_right += 1;
+
+			if (names[tmp_left] == c) {
+				LR = true;
+				break;
+			} else if (names[tmp_right] == c) {
+				LR = false;
+				break;
+			}
+
+			i++;
+		}
+		return i;
 	}
 }
+/*
+checkLR까지 구현
+
+*/
